dc38f4a7851b76b9adb9737b10f0d1f879d656cc
Uzodike Oguejiofor <reenydavidson@gmail.com>
2022-01-04 23:05:43 +0100

    updating components

--- screens/NewsDetailsScreens/HealthTodayDetailScreen.js
+++ screens/NewsDetailsScreens/HealthTodayDetailScreen.js
@@ -2,7 +2,7 @@ import React from "react";
 import { useNavigation } from "@react-navigation/native";
 import { StyleSheet, Text, View } from "react-native";
 
-export default function BusinessTodayDetailScreen() {
+export default function HealthTodayDetailScreen() {
   const navigation = useNavigation();
   const item = navigation.getState();
   return (
